3528784a30726337e43243afa52f06ff1b4faabb
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-10-30 09:56:19 +0100

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name' => 'zubiri',
+            'email' => 'zubiri@zubiri.com',
+            'password' => bcrypt('zubiri')
+        ];
+
+        $admin = [
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('zubiri')
+        ];
+        DB::table('users')->insert($user);
+        DB::table('admins')->insert($admin);
+    }
+}
